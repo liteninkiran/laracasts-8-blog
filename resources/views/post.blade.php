3c04148fdf2205c5c79e23a79833d73f21db4b8b
Kiran Anand <liteninkiran@hotmail.com>
2021-10-10 00:24:07 +0100

    Removing old files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,8 @@
 
     <article>
         <h1>{{ $post->title }}</h1>
+        <h3>By {{ $post->user->name }}</h3>
+        <p>{{ date('jS F Y', strtotime($post->published_at)) }}</p>
         <div>
             {!! $post->body !!}
         </div>
