9f905b95e3957de3b802395f9bfeb6f392b637c4
Kiran Anand <liteninkiran@hotmail.com>
2021-10-16 16:51:15 +0100

    Comment form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,20 @@
 @props(['comment'])
 
-<article class="flex {{ $comment->author->id === $comment->post->author->id ? 'bg-blue-100' : 'bg-gray-100' }} border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <a href="?author={{ $comment->author->username }}">
-            <img src="https://i.pravatar.cc/60?u={{ $comment->author->id }}" alt="" width="60" height="60" class="rounded-xl">
-        </a>
-    </div>
+<x-panel class="{{ $comment->author->id === $comment->post->author->id ? 'bg-blue-50' : 'bg-gray-50' }}">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <a href="?author={{ $comment->author->username }}">
+                <img src="https://i.pravatar.cc/60?u={{ $comment->author->id }}" alt="" width="60" height="60" class="rounded-xl">
+            </a>
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->name }}</h3>
-            <p class="text-xs">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></p>
-        </header>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->name }}</h3>
+                <p class="text-xs">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></p>
+            </header>
 
-        {!! $comment->body !!}
-    </div>
-</article>
+            {!! $comment->body !!}
+        </div>
+    </article>
+</x-panel>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -69,6 +69,30 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+
+                    @auth
+                        <x-panel>
+                            <form method="POST" action="">
+
+                                <header class="flex items-center">
+                                    <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60" height="60" class="rounded-xl">
+                                    <h2 class="ml-4">{{ auth()->user()->name }}</h2>
+                                </header>
+
+                                <div class="mt-6">
+                                    <textarea name="body" id="body" cols="30" rows="5" class="w-full text-sm focus:outline-none focus:ring" placeholder="Add a comment"></textarea>
+                                </div>
+
+                                <div class="flex justify-end pt-3">
+                                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
+                                        Post
+                                    </button>
+                                </div>
+
+                            </form>
+                        </x-panel>
+                    @endauth
+
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
                     @endforeach
