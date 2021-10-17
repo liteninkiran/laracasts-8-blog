0d0bac50de3898962cbb99198bd3be34f1f50eb5
Kiran Anand <liteninkiran@hotmail.com>
2021-10-17 09:29:24 +0100

    Subscribe to newsletter

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,14 +13,20 @@
 </style>
 
 <body style="font-family: Open Sans, sans-serif">
+
     <section class="px-6 py-8">
+
+        {{-- Navbar --}}
         <nav class="md:flex md:justify-between md:items-center">
+
+            {{-- LHS: Home Page Link --}}
             <div>
                 <a href="/">
                     <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
                 </a>
             </div>
 
+            {{-- RHS: Navigation --}}
             <div class="mt-8 md:mt-0 flex items-center">
 
                 {{-- User's Name --}}
@@ -55,35 +61,63 @@
 
                 @endauth
 
-
             </div>
+
         </nav>
 
         {{ $slot }}
 
+        {{-- Footer --}}
         <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+
+            {{-- Icon --}}
             <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+
+            {{-- Heading --}}
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
+            {{-- Subscribe --}}
             <div class="mt-10">
+
                 <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
 
-                    <form method="POST" action="#" class="lg:flex text-sm">
+                    {{-- Form --}}
+                    <form method="POST" action="/newsletter" class="lg:flex text-sm">
+
+                        @csrf
+
+                        {{-- Email Address --}}
                         <div class="lg:py-3 lg:px-5 flex items-center">
+
+                            {{-- Mail Icon --}}
                             <label for="email" class="hidden lg:inline-block">
                                 <img src="/images/mailbox-icon.svg" alt="mailbox letter">
                             </label>
 
-                            <input id="email" type="text" placeholder="Your email address"
-                                   class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                            <div>
+
+                                {{-- Text Input --}}
+                                <input id="email" name="email" type="email" placeholder="Your email address" required class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+
+                                {{-- Error Messages --}}
+                                @error('email')
+                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                                @enderror
+
+                            </div>
+        
                         </div>
 
+                        {{-- Submit Button --}}
                         <button type="submit" class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8">
                             Subscribe
                         </button>
+
                     </form>
+
                 </div>
+
             </div>
 
         </footer>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\CommentController;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,30 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::post('newsletter', function() {
+    request()->validate(['email' => ['required', 'email']]);
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+    
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us5',
+    ]);
+    
+    $listId = 'b9df78ea6e';
+
+    // $response = $mailchimp->lists->getAllLists();
+    // $response = $mailchimp->lists->getList($listId);
+    // $response = $mailchimp->lists->getListMembersInfo($listId);
+
+    try {
+        $response = $mailchimp->lists->addListMember($listId, [
+            'email_address' => request('email'),
+            'status' => 'subscribed',
+        ]);
+    } catch (\Exception $e) {
+        return redirect('/#newsletter')->with('failure', 'Could not add email');
+    }
+
+    return redirect('/')->with('success', 'You are now signed up to the Newsletter');
+});
